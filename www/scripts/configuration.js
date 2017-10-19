17d3a540d287a025b203ef3bb130d098b74b4fb1
andryromaniv <aromaniv@sfu.ca>
2017-10-19 00:27:31 -0700

    Lets go

---
+++ www/scripts/configuration.js
@@ -0,0 +1,7 @@
+"use strict";
+
+ angular.module('config', [])
+
+.constant('ENV', {name:'development',dataProvider:'FIREBASE',firebaseConfig:{apiKey:'AIzaSyB8kDpBAYwyLbAj_oAHl10gCc3GDOGjuC4',authDomain:'solodka-16657.firebaseapp.com',databaseURL:'https://solodka-16657.firebaseio.com/'},youtubeKey:'AIzaSyDael5MmCQa1GKQNKQYypmBeB08GATgSEo',facebookAppId:'',googleAppId:'',ionicCloudApiToken:'',ionicSecurityProfile:'',ionicAppId:'ab64f538',gcmId:'228071472080'})
+
+;
